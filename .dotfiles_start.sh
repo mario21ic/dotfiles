67af96645e553d1566a2cbb0dc3494d672ab2202
Mario IC <mario21ic@gmail.com>
2024-05-24 15:34:19 -0500

    updating start script

--- .dotfiles_start.sh
+++ .dotfiles_start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -xe
 
-git clone --bare https://github.com/mario21ic/dotfiles $HOME/.dotfiles
+#git clone --bare https://github.com/mario21ic/dotfiles $HOME/.dotfiles
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
-dotfiles status
-#dotfiles reset --hard
+#dotfiles status
+#dotfiles log
+dotfiles reset --hard
 
 echo "source ~/.myrc" >> ~/.bashrc
-echo "source ~/.myrc" >> ~/.zshrc
+source ~/.bashrc
+
+#echo "source ~/.myrc" >> ~/.zshrc
+#source ~/.zshrc
